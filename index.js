a556b1556aa07b3ccad4913a7341e8eb5a5de1d7
PH <paulo_phs01@hotmail.com>
2023-04-14 20:28:05 -0300

    adicionado cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 // import express from express
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require("cors")
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('SALVE MEU FI!')
